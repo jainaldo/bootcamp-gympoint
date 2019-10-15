6918d9d6c641a97d0a3351c3fe1d7f09b72aaf73
jainaldosilva <jainaldosilva@gmail.com>
2019-10-14 21:30:59 -0300

    configurando sucrase e nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ message: "hello" });
+  return res.json({ message: "hello teste dsdjasd" });
 });
 
-module.exports = routes;
+export default routes;
