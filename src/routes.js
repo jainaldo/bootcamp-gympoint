426bb7db51136aa94a39f0c3668183f8c7a970cf
jainaldosilva <jainaldosilva@gmail.com>
2019-10-18 20:27:36 -0300

    Avatar do usuário

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import StudentController from './app/controllers/StudentController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -19,8 +20,6 @@ routes.put('/users', UserController.update);
 routes.post('/students', StudentController.store);
 routes.put('/students/:id', StudentController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  res.json({ ok: 'ok' });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
